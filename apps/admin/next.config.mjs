052d134bade2cb4c2b84008f4f36b05db17651a2
Adrianwritenow <adrianwritenow@gmail.com>
2026-02-02 19:31:01 -0500

    fix: Update standalpne for mnorepo

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -5,7 +5,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   transpilePackages: ["@sparkmotion/api", "@sparkmotion/database", "@sparkmotion/ui", "@sparkmotion/auth"],
   serverExternalPackages: ["@prisma/client", "prisma"],
   experimental: {
